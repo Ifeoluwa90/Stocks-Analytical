7ea55b15c92f459bc80c88304bcdc92f0e4485fd
Ifeoluwa90 <Ifeoluwaadewoyin90@gmail.com>
2026-03-21 03:40:35 -0700

    Ngrok skip

--- main/app.py
+++ main/app.py
@@ -1,29 +1,29 @@
-# Importing req
 from flask import Flask, render_template, request
 import stocks
 
 app = Flask(__name__)
 
-# Creating Homepage
+# Skip Ngrok browser warning
+@app.after_request
+def add_ngrok_header(response):
+    response.headers["ngrok-skip-browser-warning"] = "true"
+    return response
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
-# Creating about page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-# adding a route to search
-# app.py - fetch once, pass to both #fixed
 @app.route("/search", methods=["POST"])
 def search():
     ticker = request.form["ticker"].upper()
-    stock_data = stocks.get_stock(ticker)              
-    valuation_data = stocks.get_stockvaluation(stock_data)  
+    stock_data = stocks.get_stock(ticker)
+    valuation_data = stocks.get_stockvaluation(stock_data)
     combined = {**stock_data, **valuation_data}
-    
     return render_template("results.html", data=combined)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
